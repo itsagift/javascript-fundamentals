0fbebf3491489838071f3c80b927c44e06560027
itsagift <anna.c.reynolds@gmail.com>
2022-03-15 15:31:42 -0400

    added js module file

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("Javascript is running")
+let score = 100;
---
+++ module.js
@@ -0,0 +1,7 @@
+let myName = prompt("Enter your name");
+let month = prompt("What month were you born?");
+let food = prompt("What is your favorite food?");
+
+console.log("My name is", myName);
+console.log("I was born in", month);
+console.log("My favorite food is", food);
