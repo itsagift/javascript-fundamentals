999543dccb70be05abca81fa51acf763caaccbee
itsagift <anna.c.reynolds@gmail.com>
2022-03-16 13:31:06 -0400

    added rote functions

---
+++ rote.js
@@ -0,0 +1,18 @@
+const greet = () => {
+    console.log("Hello world");
+}
+
+greet();
+
+const square = (num) => {
+    return num * num;
+}
+
+square(25);
+
+const greetByName = (str) =>{
+    console.log("Hello", str);
+}
+
+greetByName("Chase");
+
