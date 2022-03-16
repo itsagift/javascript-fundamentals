8003ba44bdbed7a5880a57ce57ec7cbee5cb24ef
itsagift <anna.c.reynolds@gmail.com>
2022-03-16 13:16:49 -0400

    functions

--- module.js
+++ module.js
@@ -1,7 +1,35 @@
-let myName = prompt("Enter your name");
-let month = prompt("What month were you born?");
-let food = prompt("What is your favorite food?");
+function quiz() {
+    let myName = prompt("Enter your name");
+    let month = prompt("What month were you born?");
+    let food = prompt("What is your favorite food?");
 
-console.log("My name is", myName);
-console.log("I was born in", month);
-console.log("My favorite food is", food);
+    console.log("My name is", myName);
+    console.log("I was born in", month);
+    console.log("My favorite food is", food);
+}
+
+const functionA = () => {
+    console.log("Function A has been called!")
+}
+
+const functionB = () => {
+    console.log("Function B has been called!")
+}
+
+const functionC = () => {
+    console.log("Function C has been called!")
+}
+
+const greet = (person) =>{
+    console.log("Hello", person)
+}
+
+const blender = (string) =>{
+    return string.split('')
+}
+
+
+const square = (num, message) => {
+    console.log("Message", message)
+    return num + num;
+}
