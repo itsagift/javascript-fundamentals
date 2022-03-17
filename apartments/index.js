24f78dbf10c382a83a84b976b67e6e13620d4249
itsagift <anna.c.reynolds@gmail.com>
2022-03-17 13:11:30 -0400

    apartment objects

---
+++ apartments/index.js
@@ -0,0 +1,30 @@
+let apt1 = {
+    bedrooms: 2, 
+    sqft: 50, 
+    bathrooms: 3, 
+    windows: 0, 
+    rent: 4500, 
+    unit: '1A'
+}
+let apt2 = {
+    bedrooms: 2, 
+    sqft: 500, 
+    bathrooms: 1, 
+    windows: 2, 
+    rent: 1500, 
+    unit: '1B'
+}
+let apt3 = {
+    bedrooms: 2, 
+    sqft: 51, 
+    bathrooms: 3, 
+    windows: 2, 
+    rent: 3400, 
+    unit: '1C'
+}
+let building = {
+    streetAddress: '74 N 7th Street, Brooklyn NY 11240',
+    laundry: false,
+    allowsPets: false,
+    apartments: [apt1, apt2, apt3]
+}
